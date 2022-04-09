ef7246e35dd475de43a053797e34081b9994f1cb
Vladimiros Makris <82274091+vladimak97@users.noreply.github.com>
2022-04-09 19:15:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace KotApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string testString = "cat dog fish bird";
+
+            string[] arrayOfS = testString.Split(",");
+
+            Dictionary<int, List<string>> dictionary = new Dictionary<int, List<string>>();
+
+            foreach (string aa in arrayOfS)
+            {
+                if (dictionary.ContainsKey())
+                {
+                    List<string> list = dictionary[key];
+                    list.Add(word);
+                }
+                else
+                {
+                    List<string> list = new List<string>();
+                    list.Add(word);
+                    dictionary.Add(key, list);
+                }
+            }
+        }
+
+        foreach(KeyValuePair<int, List<string>> kv in Dictionary)
+            {
+            Console.WriteLine(KeyValuePair.Key);
+            foreach(String word in kv.Value)
+            {
+            Console.WriteLine(word);
+            }
+
+    foreach(string wor in words)
+        {
+        Console.WriteLine(Words.ToUpper());
+        }
+
+words.Select(x => x.ToUpper()).TToList().ForEach(x => Console.WriteLine(x));
+
+int[] intTable = { 4, 2, 6, 2, 1, 5, 7, 2, 34, 6 };
+
+
+Console.WriteLine("------------------");
+
+
+intTable.Select(x => x * 2).ToList().ForEach(x => Console.WriteLine(x));
+
+intTable.Where(item => item % 2 == 0).ToList().ForEach(item => Console.WriteLine(item));
+
+intTable.Where(item => item > 4).Select(item => item * item).OrderByDescending(item => item);
+
+           arrayOfS.GroupBy(item => item.Length);
+         
+         }
+      }
+   }
+
+ 
+
+
+
+
